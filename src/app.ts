f93bb0b42b69464b23f48ef878aee34ac3658f6a
Zidane Innis <zidaneinnis@gmail.com>
2023-03-27 13:41:58 -0500

    The keyof constraint

--- src/app.ts
+++ src/app.ts
@@ -33,3 +33,12 @@ function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
   return [element, descriptionText];
 }
 console.log(countAndDescribe(["Sports", "Reading"]));
+
+function extractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value " + obj[key];
+}
+
+extractAndConvert({ name: "Zid" }, "name");
