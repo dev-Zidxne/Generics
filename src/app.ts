301704be40ee8065ceb3cf09e5c0b5d744abce08
Zidane Innis <zidaneinnis@gmail.com>
2023-03-27 17:00:17 -0500

    Generic Classes

--- src/app.ts
+++ src/app.ts
@@ -42,3 +42,39 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 extractAndConvert({ name: "Zid" }, "name");
+
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1); // -1
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Zid");
+textStorage.addItem("Max");
+textStorage.removeItem("Zid");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStroage = new DataStorage<object>();
+// const maxObj = { name: "Max" };
+// objStroage.addItem({ name: "Zid" });
+// objStroage.addItem({ name: "Max" });
+
+// objStroage.removeItem({ name: "Zid" });
+
+// console.log(objStroage.getItems());
